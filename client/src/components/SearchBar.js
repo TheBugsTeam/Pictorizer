5eabb44f25aac3cc729e038f63dd27ffc1745fcf
abelgeringer-beep <abelgeringer@gmail.com>
2021-04-02 19:37:38 +0200

    A searchbar kinézetének első változata

--- client/src/components/SearchBar.js
+++ client/src/components/SearchBar.js
@@ -42,14 +42,33 @@ const SearchBar = ({ setChosen }) => {
 
   return (
     <div>
-      <input onChange={searchInput} type="text" />
+      <input
+        onChange={searchInput}
+        type="text"
+        style={{
+          width: "350px",
+          height: "100%",
+          paddingLeft: "8px",
+          paddingTop: "6px",
+          paddingBottom: "6px",
+          borderColor: "#7d66bd",
+          backgroundColor: "#27282f",
+          borderRadius: "30px",
+          borderWidth: "5px",
+          fontSize: "20px",
+          color: "white",
+          outline: "none",
+        }}
+        placeholder="Search..."
+      />
       <StyledDiv>
         {talalat && (
           <ul>
             {talalat.map((item) => (
               <>
                 <li onClick={() => printGameData(item)}>
-                  {item.fullGameTitle}
+                  <div>{<StyledImg src={item.imageURL} alt="" />}</div>
+                  <StyledText>{item.fullGameTitle}</StyledText>
                 </li>
               </>
             ))}
@@ -60,15 +79,39 @@ const SearchBar = ({ setChosen }) => {
   );
 };
 
+const StyledText = styled.div`
+  margin-top: 15px;
+  vertical-align: middle;
+  float: middle;
+`;
+
+const StyledImg = styled.img`
+  width: 100px;
+  margin-left: 5px;
+  padding-right: 10px;
+  vertical-align: middle;
+  float: left;
+  max-height: 70px;
+`;
+
 const StyledDiv = styled.div`
+  cursor: pointer;
   position: absolute;
   ul {
+    width: 350px;
     list-style-type: none;
     display: block;
+    border-radius: 15px;
+    background-color: #363841;
+    box-shadow: 0 0 11px #111;
+    padding: 10px;
   }
 
-  li {
-    color: red;
+  ul li {
+    height: 70px;
+    color: white;
+    word-break: normal;
+    padding: 5px;
   }
 `;
 
